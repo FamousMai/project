755684afbaf9defac6a11a4f8eff0dcc15347cba
famousmai <906631095@qq.com>
2018-05-22 14:39:14 +0800

    mai || Laravel 的用户认证系统

--- PersonalIntroduction/app/Providers/AppServiceProvider.php
+++ PersonalIntroduction/app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -14,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        // 关于 PHP artisan migrate 报错问题?
+        // https://laravel-china.org/topics/9989/about-php-artisan-migrate-mistake
+        Schema::defaultStringLength(191);
     }
 
     /**
